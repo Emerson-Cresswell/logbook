18796c4f53e108e496e0b6b12ca333a590573aed
Emerson-Cresswell <emerson.cresswell@icloud.com>
2026-05-18 01:15:20 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "procedure-logbook-v10";
+const CACHE_NAME = "procedure-logbook-v11";
 
 const FILES_TO_CACHE = [
   "./",
@@ -12,7 +12,6 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
@@ -20,13 +19,13 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys()
+      .then(keys => Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
+      ))
+      .then(() => self.clients.claim())
   );
 });
 
